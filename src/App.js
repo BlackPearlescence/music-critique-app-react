348004f7476cffd171c5cdd7ffdd647ad17f6cf7
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-02-13 13:13:31 -0500

    remove unneccesary code and add a component folder to app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import {Route, Switch} from "react-router-dom"
 import React,{ useEffect } from "react-dom";
-import Home from "./Home.js";
+import Home from './components/Home.js';
 import AllSongsGallery from "./AllSongsGallery.js";
 import SongOfTheDay from "./SongOfTheDay.js";
 import SongView from "./SongView.js";
-import NavBar from "./NavBar.js";
+import NavBar from './components/NavBar.js';
 import Header from "/Header.js";
 
 function App() {
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import NavBar from './NavBar.js';
+
+function Header() {
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
+}
