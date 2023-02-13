a29a8226e92a222889b374cedfad805f40511e4b
Sani Rogers <sanirogers138@gmail.com>
2023-02-13 15:35:21 -0500

    work on songgallery

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,10 +1,15 @@
-import { Container } from "react-bootstrap";
+import CardGroup from "react-bootstrap/CardGroup";
 import SongCard from "./SongCard.js"
+import Row from "react-bootstrap/Row";
+import Container  from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form"
 function SongList({songs}){
     return(
         <div>
             <Container>
-                {songs.map(song => <SongCard key={song.id} song={song}/>)}
+                <Row>
+                    {songs.map(song => <SongCard key={song.id} song={song}/>)}
+                </Row>
             </Container>
         </div>
     );
