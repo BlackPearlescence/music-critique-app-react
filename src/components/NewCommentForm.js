b1f42d7b4abd392b504348af131706607a437414
Sani Rogers <sanirogers138@gmail.com>
2023-02-13 16:33:31 -0500

    file cleanup

---
+++ src/components/NewCommentForm.js
@@ -0,0 +1,42 @@
+import React from 'react';
+
+function NewCommentForm() {
+
+    function handleClick(event) {
+        const newComment = document.querySelector('#comment').value
+
+        fetch(``, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(newComment)
+        })
+        .then(response => response.json())
+
+        // add logic for comment
+    }
+
+    return (
+        <div>
+            <h3>Add A Comment!</h3>
+                <Form
+                onSubmit={() => {
+                    console.log('Submitting New Comment')
+                }}
+                >
+                <Form.Group widths='equal'>
+                    <Form.Input
+                    type="input"
+                    id="comment"
+                    fluid label="Comment" 
+                    placeholder="Comment" 
+                    name="Comment"/>
+                </Form.Group>
+                <Form.Button>Submit Comment</Form.Button>
+                </Form>
+        </div>
+    )
+}
+
+export default NewCommentForm;
