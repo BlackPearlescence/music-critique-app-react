69780b326076b2172b44a8562eb307e4ec7d5c02
Sani Rogers <sanirogers138@gmail.com>
2023-02-14 08:34:15 -0500

    do routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
 
-import {Route, Routes} from "react-router-dom";
+import {Route, Routes, Router} from "react-router-dom";
 import React,{ useEffect, useState } from "react";
 import Home from "./components/Home.js";
 import AllSongsGallery from "./components/AllSongsGallery.js";
 import SongOfTheDay from "./components/SongOfTheDay.js";
 import SongView from "./components/SongView.js";
-import NavBar from './components/NavBar.js';
 import Header from "./components/Header.js";
 
 function App() {
@@ -19,27 +18,18 @@ function App() {
 
   return (
     <div className="App">
-      <AllSongsGallery songs={songs}/>
+      {/* <AllSongsGallery songs={songs}/> */}
       <Header />
-      <Home />
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
 
-       {/* <Routes>
-        <Route>
-          <Home />
-        </Route>
+          <Route path="/songs" element={<AllSongsGallery songs={songs}/>}/>
 
-        <Route>
-          <AllSongsGallery songs={songs}/>
-        </Route>
+          <Route path="/songoftheday" element={<SongOfTheDay/>} />
 
-        <Route>
-          <SongOfTheDay />
-        </Route>
-
-        <Route>
-          <SongView />
-        </Route>
-      </Routes> */}
+          <Route path="/:id/view" element={<SongView />}/>
+        </Routes>
+       
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 <link
   rel="stylesheet"
@@ -14,7 +15,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Router>
     <App />
+  </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
