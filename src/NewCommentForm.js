03366f9928882ddd19dde7de051a5161a420f8ef
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2023-02-14 10:48:17 -0500

    stashing local changes

--- src/NewCommentForm.js
+++ src/NewCommentForm.js
@@ -1,40 +1,48 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form'
+import CommentCard from './CommentCard'
+import CommentList from './CommentList'
 
 function NewCommentForm() {
 
-    function handleClick(event) {
-        const newComment = document.querySelector('#comment').value
+    // function handleClick(event) {  
 
-        fetch(``, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(newComment)
-        })
-        .then(response => response.json())
+        // function handleSubmit(event) {
+        //     event.preventDefault();
 
-        // add logic for comment
-    }
+        //     const newComment = document.querySelector('#comment').value
+        // }
+
+    //     fetch(``, {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(newComment)
+    //     })
+    //     .then(response => response.json())
+
+    //     // add logic for new comment >>    setComments(prev => [...prev, newComment])
+    // }
 
     return (
         <div>
-            <h3>Add A Comment!</h3>
+
+            <CommentCard>
                 <Form
-                onSubmit={() => {
-                    console.log('Submitting New Comment')
-                }}
+                onSubmit={handleSubmit}
                 >
-                <Form.Group widths='equal'>
-                    <Form.Input
-                    type="input"
-                    id="comment"
-                    fluid label="Comment" 
-                    placeholder="Comment" 
-                    name="Comment"/>
-                </Form.Group>
-                <Form.Button>Submit Comment</Form.Button>
+                    <Form.Label>Comment Form</Form.Label>
+                    <Form.Group widths='equal'>
+                        <Form.Input
+                        type="input"
+                        id="comment"
+                        placeholder="Comment" 
+                        name="Comment"/>
+                    </Form.Group>
+                    <Form.Button onClick={handleClick}>Submit Comment</Form.Button>
                 </Form>
+            </CommentCard>
         </div>
     )
 }
