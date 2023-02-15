bc0c05accf7d2b7e104eaaf96f371ef2ba4d75d7
Sani Rogers <sanirogers138@gmail.com>
2023-02-15 11:18:43 -0500

    do newsongform progress

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,12 +3,14 @@ import SongCard from "./SongCard.js"
 import Row from "react-bootstrap/Row";
 import Container  from "react-bootstrap/Container";
 import Form from "react-bootstrap/Form"
+import NewSongForm from "./NewSongForm.js";
 function SongList({songs}){
     
     return(
         <div>
             <Container>
                 <Row>
+                    <NewSongForm/>
                     {songs.map(song => <SongCard key={song.id} song={song}/>)}
                 </Row>
             </Container>
