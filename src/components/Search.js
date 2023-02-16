692527c1c6f0c52249ed9f4bb34d72a7744a5bb0
Mayowa Ajiboye <mayaajiboye@gmail.com>
2023-02-16 14:33:23 -0500

    add scss styling to advanced search

--- src/components/Search.js
+++ src/components/Search.js
@@ -10,21 +10,23 @@ import Row from "react-bootstrap/Row"
 import { Col } from "react-bootstrap";
 function Search({songs, searchQuery, setSearchQuery, filterQuery, setFilterQuery, sortQuery, setSortQuery, genres}){
     const [isAdvanced, setIsAdvanced] = useState(false)
-    
+
     const handleAdvancedSearchClick = (e) => {
         setIsAdvanced(!isAdvanced)
     }
-    
+
     const handleSearchChange = (e) => {
         setSearchQuery(e.target.value)
     }
 
     return(
-            <InputGroup>
-                <Form.Control type="text" placeholder="Search..." onChange={handleSearchChange} value={searchQuery}/>
+        <>
+         <section>
+         <InputGroup>
+                <Form.Control  style={{ marginBottom: '2rem' , fontSize:'1.5rem'}}className='font-monospace' type="text" placeholder="Search..." onChange={handleSearchChange} value={searchQuery}/>
                 {isAdvanced ?
-                <Button onClick={handleAdvancedSearchClick}>Hide <BsPatchMinusFill/></Button>:
-                <Button onClick={handleAdvancedSearchClick }>Advanced Search <BsPatchPlusFill /></Button>}
+                <button onClick={handleAdvancedSearchClick}>Hide <BsPatchMinusFill/></button>:
+                <button onClick={handleAdvancedSearchClick }>Advanced Search <BsPatchPlusFill /></button>}
                 {isAdvanced ? <AdvancedSearch
                 filterQuery={filterQuery}
                 setFilterQuery={setFilterQuery}
@@ -33,6 +35,11 @@ function Search({songs, searchQuery, setSearchQuery, filterQuery, setFilterQuery
                 songs={songs}
                 genres={genres}/> : null}
             </InputGroup>
+
+         </section>
+        </>
+
+
     );
 }
 
